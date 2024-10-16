9bdf776628f81d699364d34eca4a3fb0582bac0a
snigdha <sunny.rangineni@gmail.com>
2024-10-16 12:18:31 -0500

    creating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "", component: HomeComponent},
+  {path: "list", component: ReservationListComponent},
+  {path: "form", component: ReservationFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
